da7c4528afacd344a72b436b5d06a927ad810aac
MuhammadShahroz <shahroxyt@gmail.com>
2024-10-21 15:32:24 +0500

    Update projectCard.tsx

--- second-portfolio-website/src/app/components/projectCard.tsx
+++ second-portfolio-website/src/app/components/projectCard.tsx
@@ -13,7 +13,7 @@ const ProjectCardComponent = ({Project}:{Project:ProjectCards}) => {
     <div className="relative bg-gradient-to-b from-[#0a6335] to-gray-500 shadow-[0_4px_10px_rgba(0,0,0,0.5)] rounded-xl p-2 flex flex-col justify-center items-center w-full md:max-w-60 hover:animate-pulse mb-4 md:mr-4 h-72">
     <h3 className="text-center mt-2 text-white font-bold mb-2 absolute top-2 text-sm">{Project.h3}</h3>
     <div className='h-36 flex justify-center items-center'>
-    <Image src={Project.src} alt="HTML" width={170} height={170} className="rounded-lg"/>
+    <Image src={Project.src} alt="Images" width={170} height={170} className="rounded-lg"/>
       </div>
     <p className="text-[11px] text-white mt-2 text-center mb-2">
     {Project.detail}
